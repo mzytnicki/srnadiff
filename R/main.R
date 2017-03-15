3c954efb994ba1d41bfb51cc25d854d06cd935af
Matthias Zytnicki <matthias.zytnicki@toulouse.inra.fr>
2017-03-15 10:22:37 +0100

    Added show method.

--- R/main.R
+++ R/main.R
@@ -1,3 +1,56 @@
+#' Overloading the show method
+#' @param object An \code{srnadiff} object.
+#'
+#' @examples
+#' dir         <- system.file("extdata", package="srnadiff", mustWork = TRUE)
+#' data        <- read.csv(file.path(dir, "data.csv"))
+#' gtfFile     <- file.path(dir, "Homo_sapiens.GRCh38.76.gtf")
+#' annotation  <- readWholeGenomeAnnotation(gtfFile)
+#' bamFiles    <- file.path(dir, data$FileName)
+#' replicates  <- data$SampleName
+#' conditions  <- factor(data$Condition)
+#' exp         <- sRNADiffExp(annotation, bamFiles, replicates, conditions)
+#' exp
+#'
+#' @export
+setMethod("show",
+          c(object = "sRNADiff"),
+          definition = function (object) {
+              cat(
+                  "Object of class sRNADiff.\n",
+                  "Annotation (length = ",
+                  length(object@annotation),
+                  ")\nBAM files (length = ",
+                  length(object@bamFileNames),
+                  "):\n",
+                  paste(' ', head(object@bamFileNames, 3)),
+                  "...\nChromosomes (length = ",
+                  length(object@chromosomes),
+                  "):\n",
+                  paste(' ', head(object@chromosomes, 3)),
+                  "...\nReplicates (length = ",
+                  length(object@replicates),
+                  "):\n",
+                  paste(' ', head(object@replicates, 3)),
+                  "...\nConditions (length = ",
+                  length(object@conditions),
+                  "):\n",
+                  paste(' ', head(object@conditions, 3)),
+                  "...\nDifferentially expressed regions (length = ",
+                  length(object@regions),
+                  ")\nSkip annotation step: ",
+                  object@skipAnnotation,
+                  "\nSkip naive step: ",
+                  object@skipNaive,
+                  "\nSkip HMM step: ",
+                  object@skipHmm,
+                  "\np-value threshold: ",
+                  object@pValue,
+                  "\n"
+                 , sep = "")
+          }
+         )
+
 #' Run the segmentation using 3 different methods, and reconciliate them.
 #'
 #' @param object An \code{srnadiff} object.
